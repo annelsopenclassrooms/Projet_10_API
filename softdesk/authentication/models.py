7fb0f1de4c2a119bd0a3f0c3467ff87ecb1c6a61
annelsopenclassrooms <eris2501@gmail.com>
2025-03-24 11:51:50 +0100

    interface d'admin ajouté

--- softdesk/authentication/models.py
+++ softdesk/authentication/models.py
@@ -4,4 +4,6 @@ from django.db import models
 from django.contrib.auth.models import AbstractUser
 
 class User(AbstractUser):
-    pass
+    age = models.PositiveIntegerField(null=True, blank=True)
+    can_be_contacted = models.BooleanField(default=False)
+    can_data_be_shared = models.BooleanField(default=False)
--- softdesk/softdesk/settings.py
+++ softdesk/softdesk/settings.py
@@ -37,9 +37,11 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'rest_framework',
     'authentication',
     'api'
 ]
+
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
